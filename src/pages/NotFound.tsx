d9bd14b087f8df533da3cda2fe606abcd9beefbc
Izumi Takayuki <r8quattrospider@gmail.com>
2026-09-20 20:40:28 +0900

    style: Project Adjustment Design

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const NotFound = () => {
-  return <div>404 Not Found</div>;
+  return (
+    <div>
+      <p className="text-3xl font-bold text-red-600 text-center">404 Not Found</p>
+    </div>
+  );
 };
 
 export default NotFound;
